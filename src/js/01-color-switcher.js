2dba4d8021dc841a197eb622cd8772f4c862bc06
Bohdan9924 <bohdanminiailo22@gmail.com>
2023-06-13 17:32:23 +0200

    completed the task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const bodyEl = document.querySelector("body");
+const startButtonEl = document.querySelector("#startButton");
+const stopButtonEl = document.querySelector("#stopButton");
 
+let intervalId;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+startButtonEl.addEventListener("click", ()=>{
+    intervalId = setInterval(()=>{
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    startButtonEl.disabled = true;
+});
+
+stopButtonEl.addEventListener('click', () => {
+    clearInterval(intervalId);
+    bodyEl.style.backgroundColor = `#FFFFFF`;
+    startButtonEl.disabled = false; 
+});
